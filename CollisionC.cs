fd80fa3c3f72a1d19520b004f77399241d66126e
alu0100615791 <alu0100615791@ull.edu.es>
2019-12-23 13:32:03 +0000

    Create CollisionC.cs

---
+++ CollisionC.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionC : MonoBehaviour
+{
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+	
+	private void OnCollisionEnter(Collision collision)
+    {
+		if(collision.gameObject.CompareTag("Terrain"))
+        {
+            return;
+        }
+		
+		GameController.ColC();
+    }
+}
